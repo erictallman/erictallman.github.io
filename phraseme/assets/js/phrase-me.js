7693e3a80dbae738ac892aa5100b2f44d4a9bec1
Eric Tallman <erictallman@users.noreply.github.com>
2016-12-30 16:27:19 -0500

    checkpoint of phraseme

--- phraseme/assets/js/phrase-me.js
+++ phraseme/assets/js/phrase-me.js
@@ -8,7 +8,20 @@
 			viewModel: null
 		};
 
+		var countdownIntervalFunction = null;
 		var runningIntervalFunction = null;
+
+		var countdownFunc = function() {
+			if (viewModel.timeLeft()>0) {
+				viewModel.timeLeft(viewModel.timeLeft()-1);
+			}
+		};
+
+		var runningFunc = function() {
+			$.ionSound.play('snap');
+		};
+
+		var words = [];
 
 		var viewModel = {
 
@@ -16,22 +29,23 @@
 
 			team2Score: ko.observable(0),
 
-			category: ko.observable(null),
-
 			word: ko.observable(null),
 
 			timeLeft: ko.observable(60),
 
-			running: ko.observable(false),
+			paused: ko.observable(true),
 
-			paused: ko.observable(false),
+			repeatSpeed: ko.observable(1500),
 
 			goStop: function() {
 				viewModel.paused(!viewModel.paused());
 			},
 
 			nextWord: function() {
-				viewModel.running(true);
+				if (viewModel.timeLeft()>1) {
+					// >1 so we can't jump past 0
+					viewModel.timeLeft(viewModel.timeLeft()-2);
+				}
 			}
 
 		};
@@ -39,10 +53,72 @@
 		var setupKnockout = function() {
 			/* hook up knockout to observables */
 			ko.applyBindings(viewModel);
+			viewModel.timeLeft.extend({rateLimit: 100});
+			viewModel.timeLeft.subscribe(function(newValue) {
+				console.log("time left: " + newValue);
+				if (newValue > 30 && newValue < 46) {
+					viewModel.repeatSpeed(1200);
+				} else if (newValue > 15 && newValue < 31) {
+					viewModel.repeatSpeed(1000);
+				} else if (newValue > 0 && newValue < 16) {
+					viewModel.repeatSpeed(500);
+				} else if (newValue <= 0) {
+					viewModel.repeatSpeed(1500);
+					viewModel.timeLeft(60);
+					viewModel.goStop();
+					$.ionSound.play('bell_ring');
+				}
+			});
+			viewModel.paused.subscribe(function(newValue) {
+				if (newValue) {
+					console.log("paused... tearing down");
+					if (countdownIntervalFunction !== null) {
+						clearTimeout(countdownIntervalFunction);
+						countdownIntervalFunction = null;
+					}
+					if (runningIntervalFunction !== null) {
+						clearTimeout(runningIntervalFunction);
+						runningIntervalFunction = null;
+					}
+				} else {
+					console.log("not paused... queuing up");
+					countdownIntervalFunction = setInterval(countdownFunc, 1000);
+					runningIntervalFunction = setInterval(runningFunc, viewModel.repeatSpeed());
+				}
+			});
+			viewModel.repeatSpeed.subscribe(function(newValue) {
+				if (runningIntervalFunction !== null) {
+					clearTimeout(runningIntervalFunction);
+					runningIntervalFunction = null;
+				}
+				if (!viewModel.paused()) {
+					runningIntervalFunction = setInterval(runningFunc, newValue);
+				}
+			});
 		};
 
 		var init = function() {
 			setupKnockout();
+			$.get('assets/data/all.txt', function(data) {
+				words = $.grep(data.split("\n"), function(line) {
+					return line!='' && line.indexOf('#')!=0;
+				});
+				console.log(words);
+			});
+			$.ionSound({
+				sounds: [
+					{
+						alias: 'bell_ring',
+						name: 'bell_ring'
+					},
+					{
+						alias: 'snap',
+						name: 'snap'
+					}
+				],
+				path: "assets/vendor/ion.sound-3.0.7/sounds/",
+				preload: true
+			});
 		};
 
 		phraseMe.viewModel = viewModel;
